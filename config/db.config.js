cc9bae9d8014a4d152e859b61ad5bdfcf67cbfc2
Nithish-CC <nyogaraj@concertcare.com>
2022-02-18 12:13:51 +0530

    commit - config

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 
 const dbConn = mysql.createConnection({
-    host: "localhost",
-    port:3306,
+    host: process.env.DB_HOST,
+    port:process.env.DB_PORT,
     user: "root",
     password: "root",
-    database: "cashflow",
+    database: process.env.DB_NAME,
 });
 
 dbConn.connect(function (error) {
