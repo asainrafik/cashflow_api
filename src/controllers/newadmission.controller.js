07c931cec323b7ea9cc18d67c4c54150ba96d7c9
asainrafik <asain.rafik@concertidc.com>
2022-02-23 18:49:36 +0530

    newadmission(23-02-2022)

--- src/controllers/newadmission.controller.js
+++ src/controllers/newadmission.controller.js
@@ -1,42 +1,42 @@
-const NewadmissionModel = require('../models/newadmission.model');
+const NewadmissionModel = require("../models/newadmission.model");
 const { Validator } = require("node-input-validator");
 
-
-exports.postadmission = (req,res)=>{
+exports.postadmission = (req, res) => {
     const v = new Validator(req.body, {
         student_name: "required",
-        DOB:"required",
-        gender:"required",
-        email:"required",
-        admission_date:"required",
-        academic_year:"required",
-        grade_id:"required",
-        section:"required",
-        previous_school_info:"required",
-        father_name:"required",
-        father_occupation:"required",
-        address:"required",
-        phone_number:"required",
-        alt_phone_number:"required",
-        student_id:"required",
-        status:"required",
+        DOB: "required",
+        gender: "required",
+        email: "required|email",
+        grade_section_id: "required",
+        previous_school_info: "required",
+        father_name: "required",
+        father_occupation: "required",
+        address: "required",
+        phone_number: "required|maxLength:10",
+        from_grade_id: "required",
+        student_type: "required",
     });
     v.check().then((matched) => {
         if (!matched) {
             res.status(422).send(v.errors);
-        }
-        else{
+        } else {
             const newRequestBody = new NewadmissionModel(req.body);
             NewadmissionModel.getNewAdmissionModel(newRequestBody, (err, newadmission) => {
-                console.log(newadmission);
                 if (err) {
                     res.status(500).send(err);
                 } else {
-                    res.status(200).send({ status: true, message: "NewAdmission inserted \u{1F973} \u{1F973}", data: newadmission });
+                    res.status(200).send({
+                        status: true,
+                        message:
+                            newadmission.IsExsist == "error"
+                                ? "Cannot Create NewAdmission\u{1F6AB}"
+                                : newadmission.IsExsist
+                                ? `NewAdmission already present \u{26D4} \u{26D4}`
+                                : `NewAdmission inserted \u{1F973} \u{1F973}`,
+                        data: newadmission,
+                    });
                 }
             });
         }
     });
-    
-}
-
+};
