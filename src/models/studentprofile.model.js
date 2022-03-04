4aaf2236e4dc3a10c406fecb56c4b422fc811205
asainrafik <98088848+asainrafik@users.noreply.github.com>
2022-03-04 15:06:07 +0530

    Update studentprofile.model.js

--- src/models/studentprofile.model.js
+++ src/models/studentprofile.model.js
@@ -8,7 +8,7 @@ var Studentprofile = function(student_profile) {
 
 Studentprofile.getStudentModel = (studentprofileReqData,result) =>{
     let admission_id= studentprofileReqData.student_admissions_id;
-    dbConn.query(`SELECT * FROM student_admissions LEFT JOIN grade_master ON student_admissions.grade_id = grade_master.grade_master_id LEFT JOIN grade_section ON student_admissions.grade_section_id = grade_section.grade_section_id where student_admissions.student_admissions_id = "${admission_id}";`,(err,res)=>{
+    dbConn.query(`SELECT * FROM student_admissions LEFT JOIN grade_master ON student_admissions.grade_id = grade_master.grade_master_id LEFT JOIN grade_section ON student_admissions.grade_section_id = grade_section.grade_section_id where student_admissions.student_id = "${admission_id}";`,(err,res)=>{
        if(res){
             result(null,res);
        }else{
