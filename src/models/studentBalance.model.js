7600927488726edefea1acd68731aba87353aca3
asainrafik <asain.rafik@concertidc.com>
2022-03-08 12:56:20 +0530

    issues changes

--- src/models/studentBalance.model.js
+++ src/models/studentBalance.model.js
@@ -39,8 +39,6 @@ studentBalance.updateStudentWithRefundFeeModel = (studentDataArr, result) => {
                             `select * from student_payment_infos where student_payment_info_id=${studentData.student_payment_info_id};`,
                             (err, res) => {
                                 let getData = res[0];
-                                let dateoftrans = new Date();
-                                let now = moment(dateoftrans).format('DD-MM-YYYY(hh:mm)');
                                 let createdObj = {
                                     student_id :getData.student_id,
                                     grade_id :Number(getData.grade_id),
@@ -97,13 +95,11 @@ studentBalance.updateStudentWithBalanceFeeModel = (studentDataArr, result) => {
                             `select * from student_payment_infos where student_payment_info_id=${studentData.student_payment_info_id};`,
                             (err, res) => {
                                 let getData = res[0];
-                                let dateoftrans = new Date();
-                                let now = moment(dateoftrans).format('DD-MM-YYYY(hh:mm)');
                                 let createdObj = {
                                     student_id :getData.student_id,
                                     grade_id :Number(getData.grade_id),
                                     section_id :Number(getData.section_id),
-                                    date_of_transcation :now,
+                                    date_of_transcation :new Date(),
                                     actual_fees :Number(getData.actual_fees),
                                     balance :Number(getData.balance),
                                     amount_paid :Number(getData.amount_paid),
