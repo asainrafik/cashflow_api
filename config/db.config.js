eda5c15ed04b5562ad3e1ad36062d336431e730d
kabil <mkabil0210@gmail.com>
2022-08-18 11:12:47 +0530

    db.config

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,12 @@
-const mysql = require("mysql2");
+const mysql = require("mysql");
 
 const dbConn = mysql.createPool({
     connectionLimit: 20,
     host: 5000,
     port: 3306,
-    user: "root",
-    password: "password",
-    database: "cashflow_test",
+    user: "cashflow",
+    password: "root",
+    database: "cashflow_client",
     debug: false,
 });
 
